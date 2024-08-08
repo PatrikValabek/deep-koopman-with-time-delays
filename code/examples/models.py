8f2b9891e690bd2ae3ac26d5bba16d8b91534431
Marek Wadinger <marekwadinger5506@gmail.com>
2024-08-08 13:22:33 +0200

    FORMAT: ruff

--- code/examples/models.py
+++ code/examples/models.py
@@ -1,22 +1,26 @@
 import numpy as np
-from neuromancer.psl.signals import step, sines, periodic, noise, walk
 from neuromancer.psl.base import ODE_NonAutonomous as ODE
 from neuromancer.psl.base import cast_backend
-import inspect, sys
+from neuromancer.psl.signals import step
+
 
 class TwoTank(ODE):
     """
     Two Tank model. with transport delay
     """
+
     @property
     def params(self):
-        variables = {'x0': [0., 0.],}
-        constants = {'ts': 10.0}
-        parameters = {'F1': 1,  
-                      'F2': 1,
-                      'k11': 0.015,
-                      'k22': 0.015,
-                      }
+        variables = {
+            "x0": [0.0, 0.0],
+        }
+        constants = {"ts": 10.0}
+        parameters = {
+            "F1": 1,
+            "F2": 1,
+            "k11": 0.015,
+            "k22": 0.015,
+        }
         meta = {}
         return variables, constants, parameters, meta
 
@@ -40,19 +44,29 @@ class TwoTank(ODE):
     def get_U(self, nsim, signal=None, **signal_kwargs):
         if signal is not None:
             return super().get_U(nsim=nsim, signal=signal, **signal_kwargs)
-        u = step(nsim=nsim, d=1, min=0.0, max=0.03,
-                 randsteps=int(np.ceil(nsim/200)), rng=self.rng)
+        u = step(
+            nsim=nsim,
+            d=1,
+            min=0.0,
+            max=0.03,
+            randsteps=int(np.ceil(nsim / 200)),
+            rng=self.rng,
+        )
         return u
 
     @cast_backend
     def equations(self, t, x, u):
-
         h1 = self.B.core.clip(x[0], 0, 10)  # States (2): level in the tanks
         h2 = self.B.core.clip(x[1], 0, 10)
         q = u[0]  # Inputs (1): valv
-        dhdt1 = q/self.F1 - self.k11/self.F1*self.B.core.sqrt(h1) #self.c1 * (1.0 - valve) * pump - self.c2 * self.B.core.sqrt(h1)
-        dhdt2 = self.k11/self.F2*self.B.core.sqrt(h1) - self.k22/self.F2*self.B.core.sqrt(h2) #self.c1 * valve * pump + self.c2 * self.B.core.sqrt(h1) - self.c2 * self.B.core.sqrt(h2)
+        dhdt1 = q / self.F1 - self.k11 / self.F1 * self.B.core.sqrt(
+            h1
+        )  # self.c1 * (1.0 - valve) * pump - self.c2 * self.B.core.sqrt(h1)
+        dhdt2 = (
+            self.k11 / self.F2 * self.B.core.sqrt(h1)
+            - self.k22 / self.F2 * self.B.core.sqrt(h2)
+        )  # self.c1 * valve * pump + self.c2 * self.B.core.sqrt(h1) - self.c2 * self.B.core.sqrt(h2)
 
         dhdt = [dhdt1, dhdt2]
-        
-        return dhdt 
+
+        return dhdt
